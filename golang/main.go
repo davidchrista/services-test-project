48b0f4d1e3e1441338ade6bf7083eb67b48ee0e9
David Christa <davidchrista@gmail.com>
2024-01-28 11:35:50 +0100

    rename

--- golang/main.go
+++ golang/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/davidchrista/go-simple-web-service/handlers"
-	"github.com/davidchrista/go-simple-web-service/mw"
+	"github.com/davidchrista/go-simple-web-service/middle"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +10,7 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.Use(mw.CorsMiddleware()).Use(mw.AuthMiddleware())
+	router.Use(middle.CorsMiddleware()).Use(middle.AuthMiddleware())
 
 	router.GET("/albums/:id", handlers.GetAlbum)
 	router.GET("/albums", handlers.GetAlbums)
--- golang/middle/auth.go
+++ golang/middle/auth.go
@@ -1,4 +1,4 @@
-package mw
+package middle
 
 import (
 	"net/http"
