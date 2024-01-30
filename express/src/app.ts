f8dd1499cf76b0bf89727f65947520b2543eec03
David Christa <davidchrista@gmail.com>
2024-01-30 17:21:57 +0100

    add express service docker stuff
    + signal handling

--- express/src/app.ts
+++ express/src/app.ts
@@ -10,7 +10,7 @@ const checkJwt = auth({
 const app = express();
 const port = 4000;
 
-app
+const server = app
   .use(cors())
   .get("/", checkJwt, (_req, res) => {
     res.send("Hello Node!");
@@ -21,3 +21,22 @@ app
   .listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
   });
+
+const signals = new Map<string, number>([
+  ["SIGHUP", 1],
+  ["SIGINT", 2],
+  ["SIGTERM", 15],
+]);
+
+const shutdown = (signal: string, value: number) => {
+  console.log("shutdown!");
+  server.close(() => {
+    console.log(`server stopped by ${signal} with value ${value}`);
+    process.exit(128 + value);
+  });
+};
+
+signals.forEach((val, signal) => {
+  process.on(signal, () => {console.log(`process received a ${signal} signal`);
+  shutdown(signal, val);})
+});
