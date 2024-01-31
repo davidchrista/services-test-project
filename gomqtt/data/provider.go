1c7b6b7566637263b072068edf9e2aa9b2938035
David Christa <davidchrista@gmail.com>
2024-01-31 21:26:11 +0100

    improve mqtt data generator

---
+++ gomqtt/data/provider.go
@@ -0,0 +1,30 @@
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Message struct {
+	ID      int    `json:"id"`
+	Message string `json:"message"`
+}
+
+func Generate() <-chan string {
+	ch := make(chan string)
+	go func() {
+		msgCount := 0
+		for ; true; msgCount += 1 {
+			delay := rand.Float32() * 5
+			time.Sleep(time.Second * time.Duration(delay))
+			var m = Message{ID: msgCount, Message: fmt.Sprintf("Duration: %f", delay)}
+			j, err := json.Marshal(m)
+			if err == nil {
+				ch <- string(j[:])
+			}
+		}
+	}()
+	return ch
+}
--- gomqtt/main.go
+++ gomqtt/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
+	"github.com/davidchrista/services-test-project/gomqtt/data"
 	"github.com/davidchrista/services-test-project/gomqtt/mqtt"
 )
 
 func main() {
 	client := mqtt.MqttClient{}
 	client.CreateMqttClient()
-	msgCount := 0
-	for ; true; msgCount += 1 {
-		client.Publish(fmt.Sprintf("message: %d!", msgCount))
-		time.Sleep(time.Second * 1)
+	ch := data.Generate()
+	for {
+		client.Publish(<-ch)
 	}
 }
