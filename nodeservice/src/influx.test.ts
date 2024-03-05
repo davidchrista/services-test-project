5db2f79466571f7b0ee22c7d09995e3cda304938
David Christa <davidchrista@gmail.com>
2024-03-05 16:20:24 +0100

    improve node time formatting

--- nodeservice/src/influx.test.ts
+++ nodeservice/src/influx.test.ts
@@ -1,4 +1,4 @@
-import { query } from "./influx";
+import { convertTime, query } from "./influx";
 
 describe("influx functionality test", () => {
   it("should create empty queries correctly", () => {
@@ -27,5 +27,13 @@ WHERE
 time >= now() - interval '3 minutes'
 AND
 "sender" IN ('gomqttemitter', 'test')`);
+  });
+});
+
+describe("influx time conversion", () => {
+  it("should convert time correctly", () => {
+    const s = "1709641075817.226410";
+    const t = convertTime(s);
+    expect(t).toBe("2024-03-05T12:17:55.817226410Z");
   });
 });
--- nodeservice/src/influx.ts
+++ nodeservice/src/influx.ts
@@ -1,7 +1,7 @@
 import { InfluxDBClient, Point } from "@influxdata/influxdb3-client";
 
 const url = "https://eu-central-1-1.aws.cloud2.influxdata.com";
-const token: string | undefined = process.env.INFLUXDB_TOKEN;;
+const token: string | undefined = process.env.INFLUXDB_TOKEN;
 const database = "services-test-project";
 
 let cli: InfluxDBClient | null = null;
@@ -35,6 +35,32 @@ time >= now() - interval '3 minutes'`;
   return q;
 }
 
+function extractDigitsAfterDecimal(num: number): string {
+  const numString = num.toFixed(9);
+  const decimalIndex = numString.indexOf(".");
+  if (decimalIndex !== -1) {
+    return numString.substring(decimalIndex + 1);
+  } else {
+    return "";
+  }
+}
+
+export function convertTime(s: string): string {
+  const milli = parseFloat(s);
+  if (Number.isNaN(milli)) {
+    return "";
+  }
+  const secs = milli / 1000;
+  const secsFull = Math.floor(secs);
+  const rest = secs - secsFull;
+  const restStr = extractDigitsAfterDecimal(rest);
+  const t = new Date(0);
+  t.setUTCSeconds(secsFull);
+  const ts = t.toISOString();
+  const fixed = ts.replace(/\.\d{3}/, `.${restStr}`);
+  return fixed;
+}
+
 export async function retrieve(senders: string[]): Promise<Entry[]> {
   const r: Entry[] = [];
   if (cli === null) {
@@ -42,7 +68,9 @@ export async function retrieve(senders: string[]): Promise<Entry[]> {
   }
   const rows = cli.query(query(senders), database);
   for await (const row of rows) {
-    r.push({sender: row["sender"], time: row["time"], value: row["value"]});
+    const time = convertTime(row["time"]);
+
+    r.push({ sender: row["sender"], time: time, value: row["value"] });
   }
   return r;
 }
