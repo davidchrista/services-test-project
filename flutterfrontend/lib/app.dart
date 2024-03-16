3e4cc6558cf8f6b8c2fdc6ac46130b7c609621c6
David Christa <davidchrista@gmail.com>
2024-03-16 21:19:09 +0100

    extract auth

--- flutterfrontend/lib/app.dart
+++ flutterfrontend/lib/app.dart
@@ -3,63 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutterfrontend/login.dart';
 import 'package:flutterfrontend/mainwidget.dart';
 
-const appScheme = 'flutterfrontend';
-
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool isBusy = false;
-  late String errorMessage;
-
-  Credentials? _credentials;
-  late Auth0 auth0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    auth0 =
-        Auth0('dev-gzm0pgbh.us.auth0.com', 'Lc5xWjq3AmxXL4Nyhp9QWspQF2psuEUm');
-    errorMessage = '';
-  }
-
-  Future<void> loginAction() async {
-    setState(() {
-      isBusy = true;
-      errorMessage = '';
-    });
-
-    try {
-      final Credentials credentials = await auth0
-          .webAuthentication(scheme: appScheme)
-          .login(audience: 'http://localhost:4000');
-
-      setState(() {
-        isBusy = false;
-        _credentials = credentials;
-      });
-    } on Exception catch (e, s) {
-      debugPrint('login error: $e - stack: $s');
-
-      setState(() {
-        isBusy = false;
-        errorMessage = e.toString();
-      });
-    }
-  }
-
-  Future<void> logoutAction() async {
-    await auth0.webAuthentication(scheme: appScheme).logout();
-
-    setState(() {
-      _credentials = null;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,12 +15,9 @@ class _AppState extends State<App> {
           title: const Text('Services Test'),
         ),
         body: Center(
-          child: isBusy
-              ? const CircularProgressIndicator()
-              : _credentials != null
-                  ? MainWidget(logoutAction, _credentials?.user,
-                      _credentials?.accessToken)
-                  : Login(loginAction, errorMessage),
+          child: Auth((Future<void> Function() logoutAction, UserProfile? user,
+                  String? accessToken) =>
+              MainWidget(logoutAction, user, accessToken)),
         ),
       ),
     );
--- flutterfrontend/lib/login.dart
+++ flutterfrontend/lib/login.dart
@@ -1,4 +1,78 @@
+import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
+
+const appScheme = 'flutterfrontend';
+
+class Auth extends StatefulWidget {
+  final Widget Function(Future<void> Function(), UserProfile?, String?)
+      createMain;
+
+  const Auth(this.createMain, {super.key});
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  bool isBusy = false;
+  late String errorMessage;
+
+  Credentials? _credentials;
+  late Auth0 auth0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    auth0 =
+        Auth0('dev-gzm0pgbh.us.auth0.com', 'Lc5xWjq3AmxXL4Nyhp9QWspQF2psuEUm');
+    errorMessage = '';
+  }
+
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      final Credentials credentials = await auth0
+          .webAuthentication(scheme: appScheme)
+          .login(audience: 'http://localhost:4000');
+
+      setState(() {
+        isBusy = false;
+        _credentials = credentials;
+      });
+    } on Exception catch (e, s) {
+      debugPrint('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<void> logoutAction() async {
+    await auth0.webAuthentication(scheme: appScheme).logout();
+
+    setState(() {
+      _credentials = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _credentials != null
+        ? widget.createMain(
+            logoutAction, _credentials?.user, _credentials?.accessToken)
+        : isBusy
+            ? const CircularProgressIndicator()
+            : Login(loginAction, errorMessage);
+  }
+}
+
 
 class Login extends StatelessWidget {
   final Future<void> Function() loginAction;
