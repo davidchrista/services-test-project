c224b38ca6a232d656ee0aecbc9c8c6df9b691be
David Christa <davidchrista@gmail.com>
2024-03-16 21:20:39 +0100

    rename file

--- flutterfrontend/lib/app.dart
+++ flutterfrontend/lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfrontend/login.dart';
+import 'package:flutterfrontend/auth.dart';
 import 'package:flutterfrontend/mainwidget.dart';
 
 class App extends StatelessWidget {
